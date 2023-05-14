7e9e8db8944da1f550acc01625eb4bf302117152
blueappleredappel <59490060+blueappleredappel@users.noreply.github.com>
2023-05-14 04:01:36 +0000

    fsa

--- serveur.js
+++ serveur.js
@@ -1,7 +1,15 @@
 const http = require("http");
 const router = require('./js/router');
 const PORT = 6161;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001:db1admin@sandbox.gwwn6rs.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 const server = http.createServer((request, response) => {
     console.log(`request recu pour  ${request.url}`);
     router.routeRequest(request, response);
@@ -11,3 +19,13 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, () => {
   console.log(`Le Serveur ecoute ${PORT}`); 
 });
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
