5ccc89c981a25e2dbd82c490e37d7482b07c4afd
blueappleredappel <59490060+blueappleredappel@users.noreply.github.com>
2023-05-14 14:08:22 +0000

    dasd

--- serveur.js
+++ serveur.js
@@ -1,15 +1,10 @@
 const http = require("http");
 const router = require('./js/router');
 const PORT = 6161;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://m001:db1admin@sandbox.gwwn6rs.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
+
 const server = http.createServer((request, response) => {
     console.log(`request recu pour  ${request.url}`);
     router.routeRequest(request, response);
@@ -19,13 +14,85 @@ const server = http.createServer((request, response) => {
 server.listen(PORT, () => {
   console.log(`Le Serveur ecoute ${PORT}`); 
 });
-async function run() {
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content - Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+app.use(express.json());
+mongoose.connect("mongodb://127.0.0.1:27017/DB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to the MongoDB database..."))
+  .catch(error => console.log("Failed to connect to the MongoDB database:", error));
+
+
+const personneSchema = new mongoose.Schema({
+  nom: String,
+  prenom: String
+});
+
+const PersonneModel = mongoose.model("personne", personneSchema);
+// Ajouter un enregistrement dans la DB (CREATE)
+app.post('/personne', async (request, response) => {
+  console.log("Route POST /personne");
+  console.log(request.body);
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+      let person = new PersonneModel(request.body);
+      let result = await person.save();
+      response.send(result);
   }
-}
-run().catch(console.dir);
+  catch (error) {
+      response.status(500).send(error);
+  }
+});
+// Obtenir la liste des enregistrements contenus dans la DB (READ)
+app.get('/personnes', async (request, response) => {
+  console.log("Route GET /personnes");
+  try {
+      let result = await PersonneModel.find().exec();
+      response.send(result);
+  }
+  catch (error) {
+      response.status(500).send(error);
+  }
+});
+// Obtenir un enregistrement en particulier dans la DB (READ)
+app.get("/personne/:id", async (request, response) => {
+  console.log("Route GET /personne/:id");
+  try {
+      let person = await PersonneModel.findById(request.params.id).exec();
+      response.send(person);
+  }
+  catch (error) {
+      response.status(500).send(error);
+  }
+});
+// Mettre à jour un enregistrement dans la DB (UPDATE)
+app.put("/personne/:id", async (request, response) => {
+  console.log("Route PUT /personne/:id");
+  console.log(request.body);
+  try {
+      let person = await PersonneModel.findById(request.params.id).exec();
+      person.set(request.body);
+      let result = await person.save();
+      response.send(result);
+  }
+  catch (error) {
+      response.status(500).send(error);
+  }
+});
+// Effacer un enregistrement (EFFACER)
+app.delete("/personne/:id", async (request, response) => {
+  try {
+      let result = await PersonneModel.deleteOne({
+          _id:
+              request.params.id
+      }).exec();
+      response.send(result);
+  }
+  catch (error) {
+      response.status(500).send(error);
+  }
+});
